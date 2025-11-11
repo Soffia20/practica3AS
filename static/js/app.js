903228d8bf8f2c4cc1ada350ae74c2747fc45456
Soffia20 <sofi.alvarez.gzz@gmail.com>
2025-11-10 23:15:40 -0600

    Update app.js post laboratorio

--- static/js/app.js
+++ static/js/app.js
@@ -683,7 +683,7 @@ app.controller("laboratorioCtrl", function ($scope, SesionService, CategoriaFact
 
         const idHora = $("#idHora").val();
 
-        $.post("/hora", {
+        $.post("/laboratorio", {
             Id_Hora: idHora,
             Hora: $("#txtHora").val(),
             Categoria: $("#txtCategoria").val()
@@ -736,3 +736,4 @@ app.controller("laboratorioCtrl", function ($scope, SesionService, CategoriaFact
 document.addEventListener("DOMContentLoaded", function (event) {
     activeMenuOption(location.hash)
 })
+
